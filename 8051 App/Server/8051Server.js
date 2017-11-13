4d7fbf8bb8567bdebf2227bbee8eece884003d4a
Marcus <marcusmallum@gmail.com>
2017-11-13 12:34:18 -0600

    The many attempts to get request working

--- 8051 App/Server/8051Server.js
+++ 8051 App/Server/8051Server.js
@@ -1,14 +1,15 @@
 const net = require('net');
 const request = require('request');
+/*const async = require('async');*/
 const Entities = require('html-entities').AllHtmlEntities;
 const entities = new Entities();
 
 /*request options*/
 let options = {
     url: 'https://opentdb.com/api.php?amount=1&type=multiple',
-    headers: {
+    /*headers: {
         'User-Agent': 'request'
-    }
+    }*/
 };
 
 
@@ -18,12 +19,62 @@ const server = net.createServer((c) => {
     console.log('client connected');
 
     /*Get Questions*/
-    let questions;
-    request(options, function(error, response, body){
-        if(!error && response.statusCode == 200){
-            questions = JSON.parse(body);
+    let questions = new Array(1);/* = getQuestions(1);*/
+    let valid = false;
+    /*async.whilst(
+        function testCondition(){
+            return !valid;
+            },
+        function makeRequest(callback){
+            request(options, callback);
+        },
+        function callback(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                questions[0] = JSON.parse(body);
+                valid = validQuestion(questions[0]);
+                console.log(valid);
+            }
         }
-    });
+    );*/
+    /*let ready = false;*/
+    /*let busy = false;*/
+    /*do {
+        /*if(!busy){
+            busy = true;*/
+    getQuestion();
+    function getQuestion(){
+        request(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                questions[0] = JSON.parse(body);
+                valid = validQuestion(questions[0]);
+                console.log(valid);
+                if(!valid){
+                    getQuestion();
+                }
+            }
+        });
+    }
+            /*request(options, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    questions[0] = JSON.parse(body);
+                    valid = validQuestion(questions[0]);
+                    console.log(valid);
+                    /*ready = true;*/
+                    /*busy = false;*/
+                    /*questions[0] = body;*/
+                    /*console.log("DONE");
+                    if(validQuestion(questions[i])){
+                        i++;
+                    }*/
+                /*}
+                /*console.log(response);*/
+            /*});
+        /*}*/
+        /*while(!ready);
+        ready = false;
+    /*console.log(questions[0]);*/
+    /*}while(!valid);*/
+
 
     c.on('end', () => {
         console.log('client disconnected');
@@ -34,16 +85,16 @@ const server = net.createServer((c) => {
         /*let answers = [entities.decode(questions.results[0].correct_answer), entities.decode(questions.results.)]*/
         /*Get a question*/
         if(chunk.toString() === "GETQ"){
-            console.log(entities.decode(questions.results[0].question));
-            c.write(entities.decode(questions.results[0].question) + "\r");
+            console.log(entities.decode(questions[0].results[0].question));
+            c.write(entities.decode(questions[0].results[0].question) + "\r");
         } /*Get correct answer*/
         /*else if(chunk.toString() === "GETC"){
             console.log(entities.decode(questions.results[0].correct_answer));
             c.write(entities.decode(questions.results[0].correct_answer) + "\r");
         }*/ /*Get incorrect answers*/
         else if(chunk.toString() === "GETA"){
-            let badAnswers = questions.results[0].incorrect_answers;
-            let answers = [entities.decode(questions.results[0].correct_answer), entities.decode(badAnswers[0]), entities.decode(badAnswers[1]), entities.decode(badAnswers[2])];
+            let badAnswers = questions[0].results[0].incorrect_answers;
+            let answers = [entities.decode(questions[0].results[0].correct_answer), entities.decode(badAnswers[0]), entities.decode(badAnswers[1]), entities.decode(badAnswers[2])];
             /*console.log(entities.decode(badAnswers[0] + " " + badAnswers[1] + " " + badAnswers[2]));
             c.write(entities.decode(badAnswers[0] + " " + badAnswers[1] + " " + badAnswers[2]) + "\r");*/
             console.log(answers);
@@ -60,48 +111,57 @@ server.listen(3000, () => {
 
 
 
-function getQuestions() {
-    return rp(options).then(function(data){
-        /*if(validQuestion(data)){
-            return data;
-        }
-        else{
-            getQuestions();
-        }*/
-        return data;
+/*function getQuestions(amount) {
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            questions[0] = JSON.parse(body);
+            valid = validQuestion(questions[0]);
+            console.log(valid);
+    /*let i = 0;
+    let questions = new Array(amount);
 
-        /*let questions = data;
-        return questions;*/
-        /*return data;*/
-    })
-        .catch(function(err){
-            console.log(err);
+    console.log("HERE");
+    while(i < amount){
+        request(options, function(error, response, body){
+            if(!error && response.statusCode == 200){
+                /*questions[i] = JSON.parse(body);*/
+                /*console.log("DONE");
+                if(validQuestion(questions[i])){
+                    i++;
+                }
+            }
+            console.log(response);
         });
-}
+    }
+    return questions;*/
+/*}*/
 
 function validQuestion(questions) {
     /*getQuestions().then(function(questions){*/
-        if(questions == null)
+    console.log("COOL");
+    if(questions == null) {
+        return false;
+    }
+    console.log("BETTER");
+    let question = entities.decode(questions.results[0].question);
+    let badAnswers = questions.results[0].incorrect_answers;
+    let answers = [entities.decode(questions.results[0].correct_answer), entities.decode(badAnswers[0]), entities.decode(badAnswers[1]), entities.decode(badAnswers[2])];
+    console.log("CLOSE");
+    /*Check Question*/
+    if(questionLength(question) > 80){
+        return false;
+    }
+    console.log("CLOSER");
+
+    /*Check Answers*/
+    for(let i = 0; i < answers.length; i++){
+        if(answers[i].length > 17)
         {
             return false;
         }
-        let question = entities.decode(questions.results[0].question);
-        let badAnswers = questions.results[0].incorrect_answers;
-        let answers = [entities.decode(questions.results[0].correct_answer), entities.decode(badAnswers[0]), entities.decode(badAnswers[1]), entities.decode(badAnswers[2])];
-
-        /*Check Question*/
-        if(questionLength(question) > 80){
-            return false;
-        }
-
-        /*Check Answers*/
-        for(let i = 0; i < answers.length; i++){
-            if(answers[i].length > 17)
-            {
-                return false;
-            }
-        }
-        return true;
+    }
+    console.log("MADE IT");
+    return true;
     /*});*/
 }
 
@@ -123,11 +183,18 @@ function questionLength(question) {
     return length;
 }
 
+/*function sleep(milliseconds) {
+    let start = new Date().getTime();
+    for (let i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds){
+            break;
+        }
+    }
+}*/
 
 
 
-
-let promiseWhile = function(condition, action) {
+/*let promiseWhile = function(condition, action) {
     let resolver = Promise.defer();
 
     let loop = function() {
@@ -140,4 +207,4 @@ let promiseWhile = function(condition, action) {
     process.nextTick(loop);
 
     return resolver.promise;
-};
+};*/
